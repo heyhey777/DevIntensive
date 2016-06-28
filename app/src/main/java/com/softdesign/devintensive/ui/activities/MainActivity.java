1fe3582e0837d150df1cc361efe347e8bf172e1f
Ekaterina <ekaterina.tretyakova1@gmail.com>
2016-06-29 01:39:29 +0300

    сделано 3е дз без звездочек и с косяками Реализовать Navigation Drawer Реализовать Collapsing Toolbar  Реализовать редактирование и сохранение пользовательских данных

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.softdesign.devintensive.ui.activities;
 import android.media.Image;
 import android.os.Handler;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,10 +15,14 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener{
 
@@ -25,6 +31,14 @@ private ImageView mCallImg;
     private CoordinatorLayout mCoordinatorLayout;
     private Toolbar mToolbar;
     private DrawerLayout mNavigationDrawer;
+    private FloatingActionButton mFab;
+    private EditText mUserPhone, mUserMail, mUserVk, mUserGit, mUserBio;
+
+    private List<EditText> mUserInfo;
+
+
+    // в записи этого не было (01:36) но без этого не работает
+    private int mCurrentEditMode = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +50,32 @@ private ImageView mCallImg;
         mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mUserPhone = (EditText) findViewById(R.id.phone_et);
+        mUserMail = (EditText) findViewById(R.id.email_et);
+        mUserVk = (EditText) findViewById(R.id.vk_et);
+        mUserGit = (EditText) findViewById(R.id.repository_et);
+        mUserBio = (EditText) findViewById(R.id.aboutmyself_et);
 
-        mCallImg.setOnClickListener(this);
+        mUserInfo = new ArrayList<>();
+        mUserInfo.add(mUserPhone);
+        mUserInfo.add(mUserMail);
+        mUserInfo.add(mUserVk);
+        mUserInfo.add(mUserGit);
+        mUserInfo.add(mUserBio);
+
+        mFab.setOnClickListener(this);
+        setupToolbar();
+        setupDrawer();
+
+
 
         if (savedInstanceState == null){
             //1st run
          //   showSnackbar("активити запускается впервые");
          } else {
-          //  showSnackbar("активити уже создавалось");
+            mCurrentEditMode = savedInstanceState.getInt(ConstantManager.EDIT_MODE_KEY, 0);
+            changeEditMode(mCurrentEditMode);
         }
     }
 
@@ -91,9 +123,16 @@ private ImageView mCallImg;
     @Override
     public void onClick(View v){
         switch (v.getId()){
-            case R.id.call_img:
-              /*  showProgress();
-                runWithDelay();*/
+            case R.id.fab:
+
+                if (mCurrentEditMode == 0) {
+                    changeEditMode(1);
+                    mCurrentEditMode=1;
+                } else{
+                    changeEditMode(0);
+                    mCurrentEditMode=0;
+                }
+
                 break;
         }
     }
@@ -101,6 +140,7 @@ private ImageView mCallImg;
     @Override
     protected void onSaveInstanceState(Bundle outState){
         super.onSaveInstanceState(outState);
+        outState.putInt(ConstantManager.EDIT_MODE_KEY, mCurrentEditMode);
     }
 
     private void showSnackbar(String message){
@@ -122,6 +162,52 @@ private ImageView mCallImg;
         ActionBar actionBar = getSupportActionBar();
         if (actionBar !=null) {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
+
+    private void setupDrawer(){
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+                public boolean onNavigationItemSelected(MenuItem item) {
+                    showSnackbar(item.getTitle().toString());
+                    item.setChecked(true);
+                    mNavigationDrawer.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+    }
+
+    /**
+     * перекл режим редактирования
+     * @param_mode если 1 режим редактирования, 0 - просмотра
+     */
+    private void changeEditMode(int mode){
+        if (mode == 1) {
+            mFab.setImageResource(R.drawable.ic_done_black_24dp);
+            for (EditText userValue : mUserInfo) {
+                userValue.setEnabled(true);
+                userValue.setFocusable(true);
+                userValue.setFocusableInTouchMode(true);
+            }
+        }else {
+            mFab.setImageResource(R.drawable.ic_create_black_24dp);
+                for (EditText userValue : mUserInfo){
+                    userValue.setEnabled(false);
+                    userValue.setFocusable(false);
+                    userValue.setFocusableInTouchMode(false);
+                }
+            }
+        }
+
+
+    private void loadUserInfoValue(){
+
+    }
+
+    private void saveUserInfoValue(){
+
+    }
+
 }
--- app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
@@ -5,4 +5,13 @@ package com.softdesign.devintensive.utils;
  */
 public interface ConstantManager {
     String TAG_PREFIX="Dev ";
+
+
+String EDIT_MODE_KEY = "EDIT_MODE_KEY";
+
+
+
+    //хз откуда
+    String COLOR_MODE_KEY="COLOR_MODE_KEY";
+
 }
